ae394234c4c3c7912d9e0a1b00aec99d3a7d3a0f
Brian Mix <brian@brokerbin.com>
2022-07-27 12:02:27 -0500

    bugfix for select

--- resources/views/components/questions/select.blade.php
+++ resources/views/components/questions/select.blade.php
@@ -1,16 +1,16 @@
 @props(['disabled' => false])
 
 <div class="w-full p-3 flex flex-col">
-    <x-form-label for="{{ $name }}" :value="__($label)" :tooltip="$tooltip ?? null" class="{{ $errors->has($name) ? 'text-red-600' : '' }}"></x-form-label>
-{{--    <select id="{{ $name }}" name="{{ $name }}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'.($errors->has($name)?' text-red-600 border-red-600':'')]) !!}>--}}
-{{--        <option value="">Choose ...</option>--}}
-{{--        @foreach($options as $val=>$label)--}}
-{{--            <option {{ $val == old($name) || $val === $value ? 'selected="selected"' : '' }} value="{{ $val }}">--}}
-{{--                {{ $label }}--}}
-{{--            </option>--}}
-{{--        @endforeach--}}
-{{--    </select>--}}
-    <x-form-select id="{{ $name }}" name="{{ $name }}" :options="$options" :value="$value"></x-form-select>
+    <x-form-label :for="$name" :value="__($label)" :tooltip="$tooltip ?? null" :class="$errors->has($name) ? 'text-red-600' : ''"></x-form-label>
+
+    <x-form-select :id="$name" :name="$name" :value="$value" :class="$errors->has($name)?' text-red-600 border-red-600':''">
+        <option value="">Choose ...</option>
+        @foreach($options as $val=>$label)
+            <option {{ $val == old($name) || $val === $value ? 'selected="selected"' : '' }} value="{{ $val }}">
+                {{ $label }}
+            </option>
+        @endforeach
+    </x-form-select>
     @if($errors->has($name))
         @foreach($errors->get($name) as $error)
             <div class="text-sm text-red-600">{{ $error }}</div>
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,8 +1,5 @@
-<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'.($errors->has($name)?' text-red-600 border-red-600':'')]) !!}>
-    <option value="">Choose ...</option>
-    @foreach($options as $val=>$label)
-        <option {{ $val == old($name) || $val === $value ? 'selected="selected"' : '' }} value="{{ $val }}">
-            {{ $label }}
-        </option>
-    @endforeach
+@props(['disabled' => false])
+
+<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) !!}>
+    {{ $slot }}
 </select>
